d46147022dc4ecb0858b8c5a3f9bceb267aae4ac
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-06-01 11:16:56 +0300

    Init

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    public String name;
+    public int maxTemperature;
+    int minTemperature;
+    int currentTemperature;
+    boolean on;
+}
---
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Conditioner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+
+//    @Test
+//    public void shouldInitFieldToZero(){
+//        Conditioner conditioner = new Conditioner();
+//        assertNull(conditioner.name);
+//        assertEquals(0, conditioner.maxTemperature);
+//
+//
+//    }
+
+}
